af6bf19756eee7039b2953c93e315aae18e991e3
Christopher Lüken-Winkels <b381737@mlogin101.hpc.dkrz.de>
2022-04-07 16:03:12 +0200

    many additions in flex_release

--- flex_release/prepare_release.py
+++ flex_release/prepare_release.py
@@ -23,56 +23,67 @@ if __name__ == "__main__":
     parser.add_argument("--split", type=str, help="If and how to split the output release files. 'station' to split according the station, else int n to split into n parts. (Number of releases should be devisable by n)")
     args = parser.parse_args()
     
-    with open(args.config) as f:
-        config = dict(yaml.full_load(f))
+    if os.path.isdir(args.config):
+        dir_path = args.config
+        file_names = os.listdir(dir_path)
+    else:
+        dir_path, file_names = args.config.rsplit("/", 1)
+        file_names = [file_names]
     
-    zkind = config["zkind"]
-    mass = config["mass"]
+    for file in file_names:
+        file_path = os.path.join(dir_path, file)
+        if len(file_names) > 1:
+            args.out_name = file.split(".")[0]
+        
+        with open(file_path) as f:
+            config = dict(yaml.full_load(f))
 
-    height_levels, part_nums = setup_column(config)
-    coords = setup_coords(config)
-    times = setup_times(config)
+        zkind = config["zkind"]
+        mass = config["mass"]
 
-    assert len(coords) == len(times), f'Coords and times have the same number of elements. Your input: len(coords)={len(coords)}, len(times)={len(times)}'
+        height_levels, part_nums = setup_column(config)
+        coords = setup_coords(config)
+        times = setup_times(config)
 
-    RELEASES = load_header(config["species"])
+        assert len(coords) == len(times), f'Coords and times have the same number of elements. Your input: len(coords)={len(coords)}, len(times)={len(times)}'
 
-    if config["discrete"]:
-        height_levels = height_levels[1:]
+        RELEASES = load_header(config["species"])
 
-    release_counter = 0
-    save_counter = 0
-    release_number = len(coords)*len(part_nums)
+        if config["discrete"]:
+            height_levels = height_levels[1:]
 
-    split = False
-    split_by = None
-    if args.split is not None:
-        if args.split == "station":
-            split = True
-            split_by = "station"
-        else:
-            split = True
-            split_by = int(args.split)
-            assert release_number % split_by == 0, f"If int is given for split argument it has to be denominator of number of releases ({release_number})"
+        release_counter = 0
+        save_counter = 0
+        release_number = len(coords)*len(part_nums)
 
-    for i, ((lon1, lon2, lat1, lat2), (date1, time1, date2, time2)) in enumerate(zip(coords, times)):
-        for j, parts in enumerate(part_nums):
-            comment = f'"coords:{(lon1, lon2, lat1, lat2)}, height:{height_levels[j]}, time:{(date1, time1, date2, time2)}"'
-            z1 = height_levels[j]
-            z2 = height_levels[j if config["discrete"] else j+1]
-            RELEASES.extend(write_to_dummy(date1, time1, date2, time2, lon1, lon2,
-                lat1, lat2, z1, z2, zkind, mass, parts, comment))
-            release_counter += 1
-            if split and isinstance(split_by, int) and release_number and release_counter%(release_number//split_by)==0:
-                save_release(args.out_dir, f"{args.out_name}_{save_counter}", RELEASES, args.config)
+        split = False
+        split_by = None
+        if args.split is not None:
+            if args.split == "station":
+                split = True
+                split_by = "station"
+            else:
+                split = True
+                split_by = int(args.split)
+                assert release_number % split_by == 0, f"If int is given for split argument it has to be denominator of number of releases ({release_number})"
+
+        for i, ((lon1, lon2, lat1, lat2), (date1, time1, date2, time2)) in enumerate(zip(coords, times)):
+            for j, parts in enumerate(part_nums):
+                comment = f'"coords:{(lon1, lon2, lat1, lat2)}, height:{height_levels[j]}, time:{(date1, time1, date2, time2)}"'
+                z1 = height_levels[j]
+                z2 = height_levels[j if config["discrete"] else j+1]
+                RELEASES.extend(write_to_dummy(date1, time1, date2, time2, lon1, lon2,
+                    lat1, lat2, z1, z2, zkind, mass, parts, comment))
+                release_counter += 1
+                if split and isinstance(split_by, int) and release_number and release_counter%(release_number//split_by)==0:
+                    save_release(args.out_dir, f"{args.out_name}_{save_counter}", RELEASES, args.config)
+                    save_counter +=1
+                    RELEASES = load_header(config["species"])
+            if split and split_by == "station":
+                save_release(args.out_dir, f"{args.out_name}_{save_counter}", RELEASES, file_path)
                 save_counter +=1
                 RELEASES = load_header(config["species"])
-        if split and split_by == "station":
-            save_release(args.out_dir, f"{args.out_name}_{save_counter}", RELEASES, args.config)
-            save_counter +=1
-            RELEASES = load_header(config["species"])
+        print(f"Save destination: {os.path.join(args.out_dir, args.out_name)}")
+        print(f"Total number of particles: {np.sum(part_nums)*len(coords)}")
 
-    print(f"Total number of particles: {np.sum(part_nums)*len(coords)}")
-    
-    save_release(args.out_dir, args.out_name, RELEASES, args.config) if not split else None
-    
+        save_release(args.out_dir, args.out_name, RELEASES, file_path) if not split else None
--- flex_release/utils.py
+++ flex_release/utils.py
@@ -1,6 +1,7 @@
 ### Utilities for column receptor setup ###
 
 import numpy as np
+import yaml
 
 def pressure_factor(
     h,
@@ -207,3 +208,34 @@ def setup_column(config):
     elif config["column_mode"] == "pressure_wu":
         setup = PressureWuColumnSetup(config)
     return setup.height_levels, setup.part_nums
+
+def config_total_parts(config_path, output_path, total_parts):
+    with open(config_path) as f:
+        config = dict(yaml.full_load(f))
+    
+    if config["column_mode"] == "wu":
+        levels = 0
+        for i, region in enumerate(config["regions"][1:]):
+            dh = config["dh"][i]
+            levels += (region-config["regions"][i])*1e3//dh
+        config["dn"] = int(total_parts/levels)
+
+    elif config["column_mode"] == "pressure":
+        assert len(config["regions"]) == 2, "For pressure setup use only one region [start, end]"
+        config["lowest_region_n"] = int(total_parts)
+    
+    elif config["column_mode"] == "pressure_wu":
+        level_list = []
+        levels = 0
+        for i, region in enumerate(config["regions"][1:]):
+            dh = config["dh"][i]
+            level_list.append(region*1e3//dh)
+            levels += region*1e3//dh
+        level_arr = np.array(level_list)
+        region_n = total_parts * level_arr/levels
+        config["region_n"] = list(region_n.astype(int))
+
+    with open(output_path, "w") as f:
+        yaml.dump(config, f, default_flow_style=False)
+    print(f"Saved to {output_path}.")
+    
